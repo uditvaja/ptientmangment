bfd8d4fdd83bb96a020331d98ab429a6de653376
udit <vajaudit91@gmail.com>
2024-10-03 02:31:37 +0530

    register page

--- Frontend/src/validation/AuthValidation.js
+++ Frontend/src/validation/AuthValidation.js
@@ -16,7 +16,48 @@ export const forgotPasswordValidationSchema = Yup.object({
   .email('Please enter a valid email address (e.g. example@example.com)')
     .required('Email is required')
 });
+// register validation
+export const registerValidationSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'First Name is too short!')
+    .max(50, 'First Name is too long!')
+    .required('First Name is required'),
+  
+  lastName: Yup.string()
+    .min(2, 'Last Name is too short!')
+    .max(50, 'Last Name is too long!')
+    .required('Last Name is required'),
+  
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
 
+  phoneNumber: Yup.string()
+    .matches(/^[0-9]+$/, 'Phone number is not valid')
+    .min(10, 'Phone number must be at least 10 digits')
+    .max(15, 'Phone number can be up to 15 digits')
+    .required('Phone number is required'),
+
+  country: Yup.string()
+    .required('Country is required'),
+
+  state: Yup.string()
+    .required('State is required'),
+
+  city: Yup.string()
+    .required('City is required'),
+
+  hospital: Yup.string()
+    .required('Please select a hospital'),
+
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+});
 // Otp Validation
 export const otpValidationSchema = Yup.object().shape({
   otp: Yup.array()
